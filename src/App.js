6f6f6aa4335ae6a1ab57381884cd5c1642a3e28b
Annie <anniekao@posteo.net>
2022-02-15 12:53:03 -0500

    add calls to MOMA Collection API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import momaService from './services/momaService.js'
 
 export default function App() {
+  const [queryData, setQueryData] = useState(null)
+  const [objectData, setObjectData] = useState(null)
+  
+  useEffect(() => {
+    async function getObjects() {
+      const objects = await momaService.getDepartmentCollection('21')
+      setQueryData(hasImages)
+    }
+    getObjects()
+  }, [])
+
+  useEffect(() => {
+    async function getObject() {
+      const randomNum = Math.round(Math.random() * queryData.length)
+      const objectId = queryData[randomNum]
+      const object = await momaService.getObject(objectId)
+
+      setObjectData(object)
+    }
+    getObject()
+  }, [queryData])
+  
+  const renderObject = () => (
+    <div>
+      <h1>{objectData.title}</h1>
+      <img src={objectData.primaryImageSmall} />
+    </div>
+  )
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {objectData && renderObject()}
     </div>
   )
 }
